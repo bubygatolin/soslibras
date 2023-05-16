4eabbddc34280249358dfa7eeda487ce8990c4fe
Buby Gatolin <bubygat@gmail.com>
2023-05-16 13:17:17 -0300

    colorNav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,24 +63,24 @@ const Navbar = () => {
       <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
         <div className={
             nav
-              ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+              ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#225c60] p-10 ease-in duration-500'
               : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
           }>
           <div>
             <div className='flex w-full items-center justify-between'>
               <Image className=' rounded-2xl' src={NavLogo} alt='/' width={150} height={40} />
               <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
-                <AiOutlineClose size={25}/>
+                <AiOutlineClose size={25} color='#8FD2D6'/>
               </div>
             </div>
             <div className='border-b border-gray-300 my-4'>
-              <p className='w-[85%] md:w-[90%] py-4'>Tire suas dúvidas!</p>
+              <p className='w-[85%] md:w-[90%] py-4  text-[#8FD2D6]'>Tire suas dúvidas!</p>
             </div>
           </div>
           <div className='py-4 flex flex-col'>
-            <ul className='uppercase'>
+            <ul className='uppercase  text-[#8FD2D6]'>
               <Link href='/' legacyBehavior>
-                <li onClick={()=> setNav(false)} className='py-4 text-sm'>Home</li>
+                <li onClick={()=> setNav(false)} className='py-4 text-sm '>Home</li>
               </Link>
               <Link href='/#about' legacyBehavior>
                 <li onClick={()=> setNav(false)} className='py-4 text-sm'>Sobre</li>
@@ -95,9 +95,9 @@ const Navbar = () => {
                 <li onClick={()=> setNav(false)} className='py-4 text-sm'>Contato</li>
               </Link>
             </ul>
-            <div className='pt-40'>
-              <p className='uppercase tracking-widest text- [#5651e5]'>Rede sociais</p>
-              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+            <div className='pt-10'>
+              <p className='uppercase tracking-widest text-[#8FD2D6]'>Rede sociais</p>
+              <div className='flex items-center justify-between my-4 w-full sm:w-[80%]  text-[#8FD2D6]'>
                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
                   <FaLinkedinIn />
                 </div>
